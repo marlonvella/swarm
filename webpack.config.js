58ff381c6afc2900b16e45567cc61d9e9ff751fd
Marlon Vella <marlon.vella@live.com>
2018-12-12 15:11:15 +0100

    Added Main content layout

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,60 @@ const path = require('path');
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: './bundle.js'
-    },
-    devServer: {
-        contentBase: './dist'
-    },
+	entry: ['babel-polyfill', './src/index.js'],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: './bundle.js'
+	},
+	devServer: {
+		contentBase: './dist'
+	},
 
-    module: {
-        rules: [{
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader'
-                }
-            },
-            {
-                test: /\.s[c|a]ss$/,
-                use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-            }
-        ]
-    },
+	module: {
+		rules: [{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader'
+				}
+			},
+			{
+				test: /\.s[c|a]ss$/,
+				use: ['style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [{
+					loader: 'file-loader',
+					options: {
+						name: '[name].[ext]',
+						outputPath: 'images/'
+					  }
+				}]
+			}
+		]
+	},
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'style.css',
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery',
-            Popper: ['popper.js', 'default']
-        })
-    ]
-};
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: 'index.html',
+			template: './src/index.html'
+		}),
+		new MiniCssExtractPlugin({
+			filename: 'style.css',
+		}),
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery',
+			'window.jQuery': 'jquery',
+			Popper: ['popper.js', 'default']
+		}),
+		new CopyWebpackPlugin([
+            {from:'src/assets/img',to:'images'} 
+        ])
+	]
+}
